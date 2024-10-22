fc9895fa4e021cbe5931580f685d4cdcec9ddf41
kareem-kamel <kareem.elfeky@ejust.edu.eg>
2024-10-22 06:44:00 +0300

    ubdate in class custom action button

--- lib/core/widgets/custom_action_button.dart
+++ lib/core/widgets/custom_action_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:mediconda/core/managers/color_manager.dart';
 
 import '../managers/font_style_manager.dart';
 
@@ -7,6 +8,7 @@ class CustomActionButton extends StatelessWidget {
   final Color backgroundColor;
   final Color textColor;
   final String? svgPath;
+  final Color? iconColor;
   final String? appLink;
   const CustomActionButton({
     super.key,
@@ -15,6 +17,7 @@ class CustomActionButton extends StatelessWidget {
     required this.textColor,
     this.svgPath, // Made this optional
     this.appLink,
+    this.iconColor
 
   });
 
@@ -25,35 +28,40 @@ class CustomActionButton extends StatelessWidget {
       onTap: () {
         // Handle tap action
       },
-      child: Container(
-        height: 80,
-        decoration: BoxDecoration(
-          color: backgroundColor,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Check if svgPath is not null before using it
-            if (svgPath != null)
-              Image.asset(
-                svgPath!,
-                height: 50, // Adjusted size for visibility
-                width: 50,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Container(
+          height: 80,
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Check if svgPath is not null before using it
+              if (svgPath != null)
+                Image.asset(
+                  svgPath!,
+                  height: 50, // Adjusted size for visibility
+                  width: 35,
+                  color:iconColor,
+                ),
+              const SizedBox(width: 10),
+              Text(
+                text,
+                style: FontStyleManager.getOverLockRegular(color: textColor, fontSize: 30),
               ),
-            const SizedBox(width: 8),
-            Text(
-              text,
-              style: FontStyleManager.getOverLockRegular(color: textColor, fontSize: 30),
-            ),
-            const SizedBox(width: 8),
-            if (svgPath != null)
-              Image.asset(
-                svgPath!,
-                height: 50, // Adjusted size for visibility
-                width: 50,
-              ),
-          ],
+              const SizedBox(width: 10),
+              if (svgPath != null)
+                Image.asset(
+                  svgPath!,
+                  height: 50, // Adjusted size for visibility
+                  width: 35,
+                    color:iconColor
+                ),
+            ],
+          ),
         ),
       ),
     );
